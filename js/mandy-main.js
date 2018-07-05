9bf270a281148d451d00870eae166611acb7cc26
Generosa Litton <glcodeworks@gmail.com>
2018-07-05 14:45:19 -0500

    minified css and js

--- js/mandy-main.js
+++ js/mandy-main.js
@@ -1,80 +1,79 @@
-"use strict"
+"use strict";
 
-$(document).ready(function(){
-    // Back to top js
-     $(window).scroll(function () {
-            if ($(this).scrollTop() > 50) {
-                $('#back-to-top').fadeIn();
-            } else {
-                $('#back-to-top').fadeOut();
-            }
-        });
-        // scroll body to 0px on click
-        $('#back-to-top').click(function () {
-            $('#back-to-top').tooltip('hide');
-            $('body,html').animate({
-                scrollTop: 0
-            }, 800);
-            return false;
-        });
-        
-        $('#back-to-top').tooltip('show');
+$(document).ready(function() {
+  // Back to top js
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 50) {
+      $("#back-to-top").fadeIn();
+    } else {
+      $("#back-to-top").fadeOut();
+    }
+  });
+  // scroll body to 0px on click
+  $("#back-to-top").click(function() {
+    $("#back-to-top").tooltip("hide");
+    $("body,html").animate(
+      {
+        scrollTop: 0
+      },
+      800
+    );
+    return false;
+  });
 
-     // Modals   
-    $('.launch-modal').on('click', function(e){
-        e.preventDefault();
-        $( '#' + $(this).data('modal-id') ).modal();
-    });  
+  $("#back-to-top").tooltip("show");
 
-    // Stops YouTube video when modal window is closed
-    // Alone Together
-    $('.close-alone').on('click', function(){
-        var video = $('.alone').attr('src');
-          $('.alone').attr('src','');
-          $('.alone').attr('src', video);
-        }
-      )
-    // Rainy Day
-    $('.close-rainy').on('click', function(){
-        var rainyDay = $('.rainy').attr('src');
-          $('.rainy').attr('src','');
-          $('.rainy').attr('src', rainyDay);
-        }
-      )        
+  // Modals
+  $(".launch-modal").on("click", function(e) {
+    e.preventDefault();
+    $("#" + $(this).data("modal-id")).modal();
+  });
 
-    // Turns off audio when modal window is closed
-    // Ornithology
-    $('#modal-ornithology').on('hide.bs.modal', function () {
+  // Stops YouTube video when modal window is closed
+  // Alone Together
+  $(".close-alone").on("click", function() {
+    var video = $(".alone").attr("src");
+    $(".alone").attr("src", "");
+    $(".alone").attr("src", video);
+  });
+  // Rainy Day
+  $(".close-rainy").on("click", function() {
+    var rainyDay = $(".rainy").attr("src");
+    $(".rainy").attr("src", "");
+    $(".rainy").attr("src", rainyDay);
+  });
+
+  // Turns off audio when modal window is closed
+  // Ornithology
+  $("#modal-ornithology").on("hide.bs.modal", function() {
     // for each audio tag
-      $('audio').each(function(){
+    $("audio").each(function() {
       this.pause(); // Stop playing
       this.currentTime = 0; // Reset time
-      });
-    })
-    // Stolen Moments
-    $('#modal-stolen').on('hide.bs.modal', function () {
-    // for each audio tag
-      $('audio').each(function(){
-      this.pause(); // Stop playing
-      this.currentTime = 0; // Reset time
-      });
-    })
-    // Free Peace
-    $('#modal-free').on('hide.bs.modal', function () {
-    // for each audio tag
-      $('audio').each(function(){
-      this.pause(); // Stop playing
-      this.currentTime = 0; // Reset time
-      });
-    })
-
-    // Main Carousel
-    $('.carousel').carousel({
-      interval: 8000 //changes the speed
     });
+  });
+  // Stolen Moments
+  $("#modal-stolen").on("hide.bs.modal", function() {
+    // for each audio tag
+    $("audio").each(function() {
+      this.pause(); // Stop playing
+      this.currentTime = 0; // Reset time
+    });
+  });
+  // Free Peace
+  $("#modal-free").on("hide.bs.modal", function() {
+    // for each audio tag
+    $("audio").each(function() {
+      this.pause(); // Stop playing
+      this.currentTime = 0; // Reset time
+    });
+  });
 
-    // Set default volume of audio to match video
-    document.getElementById("audioStolen").volume = 0.1;
+  // Main Carousel
+  $(".carousel").carousel({
+    interval: 8000 //changes the speed
+  });
 
-  
+  // Set default volume of audio to match video
+  document.getElementById("audioStolen").volume = 0.1;
 });
